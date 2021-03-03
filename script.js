d262ceb6069bed6286882f3cc4cfc693d87f97d0
cybershepherd <g.modzelewski@protonmail.com>
2021-03-03 13:38:31 +0100

    Code cleaning

--- script.js
+++ script.js
@@ -1,52 +1,50 @@
 var modalDisplayed = false;
 
-const positiveReponse = "That's great. All the best to ya";
+const positiveResponse = "That's great. All the best to ya";
 const negativeResponse = "That's a shame. I hope it will get better :)"
 
-const test = function(value) { alert(value); }
-
-const fn_pressMe = function() {
+const respDiv = $('#response');
+const pressMe = function() {
 	showModal($('#defmodal'));
 }
-
-const fn_hideAllModals = function() {
+const hideAllModals = function() {
 	hideModal($('.modal'));
 }
-
-const fn_positiveResponse = function() {
-	fn_hideAllModals;
-	$('#response').html(positiveReponse);
-	$('#response').fadeIn(200);
-	fadeOutResponse();
+const respDivPositive = function(){
+	setRespDivValue(positiveResponse);
+}
+const respDivNegative = function(){
+	setRespDivValue(negativeResponse);
 }
 
-const fn_negativeResponse = function() {
-	fn_hideAllModals;
-	$('#response').html(negativeResponse);
-	$('#response').fadeIn(200);
+
+$('#btn-pressMe').click(pressMe);
+$('#overlay').click(hideAllModals);
+$('.modal button').click(hideAllModals);
+$('#defmodal').children().eq(2).children().eq(0).click(respDivPositive);
+$('#defmodal').children().eq(2).children().eq(1).click(respDivNegative);
+
+function setRespDivValue(value){
+	hideAllModals;
+	respDiv.text(value);
+	respDiv.fadeIn(200);
 	fadeOutResponse();
 }
 
 function fadeOutResponse(){
-	setTimeout(function(){$('#response').fadeOut(1500);}, 1000);
+	setTimeout(function(){respDiv.fadeOut(1500);}, 1000);
 }
-
-$('#btn-pressMe').click(fn_pressMe);
-$('#overlay').click(fn_hideAllModals);
-$('.modal button').click(fn_hideAllModals);
-$('#defmodal').children().eq(2).children().eq(0).click(fn_positiveResponse);
-$('#defmodal').children().eq(2).children().eq(1).click(fn_negativeResponse);
 
 function showModal(modal){
 	if(modalDisplayed) return;
 	$('#overlay').toggleClass('active');
-	modal.css('display', 'block');
+	modal.toggleClass('active');
 	modalDisplayed = true;
 }
 
 function hideModal(modal){
 	if(!modalDisplayed) return;
 	$('#overlay').toggleClass('active');
-	modal.css('display', 'none');
+	modal.toggleClass('active');
 	modalDisplayed = false;
 }
